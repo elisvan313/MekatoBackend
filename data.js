86aad178e8523236bdfb720294a32689a4687362
elisvan313 <elisvanr@uninorte.edu.co>
2022-03-10 20:22:24 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const data ={
+    message:"hola",
+    missions:[],
+    players:[],
+    characters:[],
+    mission_objectives:[],
+    items:[],
+    models_3d:[],
+    images_2d:[],
+    player_characters:[],
+    character_stats:[]
+}
+
+module.exports=data
